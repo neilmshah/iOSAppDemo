84893ae5c7579f64547b56aa2e78aa4920d9e1d2
Neil Shah <neilmshah01@gmail.com>
2018-09-14 11:41:02 -0700

    inital commit

--- appDemo/ViewController.swift
+++ appDemo/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var helloText: UITextField!
+    var backgroundColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        backgroundColor = view.backgroundColor
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeColor(_ sender: Any) {
+        helloLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        if(helloText.text?.isEmpty)!{
+            helloLabel.text = "Goodbye!"
+        } else {
+            helloLabel.text = helloText.text
+            helloText.text = ""
+        }
+        view.endEditing(true)
+    }
+    
+    @IBAction func resetGesture(_ sender: Any) {
+        helloLabel.text = "Hello from Charlie!"
+        helloLabel.textColor = UIColor.white
+        view.backgroundColor = backgroundColor
+    }
 }
 
